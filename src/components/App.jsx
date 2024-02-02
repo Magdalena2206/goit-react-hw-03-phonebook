a21385571eb1ce558ae17c0af90e4e8d5ce8728d
Magdalena Uszynska <magdalenauszynska1@gmail.com>
2024-02-02 18:37:14 +0100

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import { nanoid } from 'nanoid';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactsList/ContactList';
-c
+
 export class App extends Component {
   state = {
     contacts: [
